67e4501454f78cda9f778a9328686732e08e7bc0
capTsubasa10 <ashwinraaj1093@gmail.com>
2022-12-20 18:03:41 +0530

    Jenkins Project

---
+++ src/test/java/Jenkins/test/Project1.java
@@ -0,0 +1,22 @@
+package Jenkins.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Project1 {
+	@Test
+	public void browserLaunch() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.close();
+
+	}
+
+
+}
